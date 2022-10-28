4e18c1184680d3e292cc7879a8a49e1fd18d0e94
Нурлан <@->
2022-10-28 22:07:00 +0400

    Добавьте файлы проекта.

---
+++ mass3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace mass3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] k = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 22 };
+            int mass;
+            mass = k.Max();
+            Console.WriteLine(mass);
+            Console.ReadLine();
+
+        }
+    }
+}
